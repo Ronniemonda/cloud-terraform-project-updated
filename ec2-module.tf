57d86c00f0bee693de142d102debdc7e97fca359
ronnie <mondaronnie@gmail.com>
2023-01-30 16:39:42 +0300

    Ronnie updated the code by adding the data-ec2 and module

---
+++ ec2-module.tf
@@ -0,0 +1,14 @@
+resource "aws_instance"  "Monda-application-server" {
+    ami = data.aws_ami.amazon-linux-2.id
+    instance_type = "t2-small"
+    key_name = "Monda-application"
+    subnet_id = aws_subnet.public-subnet-1.id
+    associate_public_ip_address = true
+    vpc_security_group_ids = aws_security_group.instance-web-server.id
+    
+    tags = {
+
+        name = "Monda-application-server"
+    }
+  
+}
---
+++ security-group.tf
@@ -0,0 +1,12 @@
+resource "aws_security_group" "instance-web-server" {
+    name = "allow-ssh-web-server"
+    description = "allow ssh traffic web server"
+    vpc_id = aws_vpc.main-VPC.id 
+
+    ingress =   {
+      cidr_blocks = [ "0.0.0.0-0" ]
+      from_port = 22
+      protocol = "tcp"
+      to_port = 22
+    }
+}
