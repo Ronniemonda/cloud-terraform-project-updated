ebc34506b4f87d05f3cb7b85f4264ce4cbeb53e2
ronnie <mondaronnie@gmail.com>
2023-01-31 07:55:19 +0300

    security group code updated

--- security-group.tf
+++ security-group.tf
@@ -3,12 +3,24 @@ resource "aws_security_group" "instance-web-server" {
     description = "allow ssh traffic web server"
     vpc_id = aws_vpc.main-VPC.id 
 
-    ingress = [ {
-      cidr_blocks = [ "0.0.0.0-0" ]
-      from_port = 22
-      protocol = "tcp"
-      to_port =22
-    } ]
+      ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
