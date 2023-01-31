fbdd92b5b288595dd01d28e1536bfb35279657e3
ronnie <mondaronnie@gmail.com>
2023-01-31 15:29:52 +0300

    ronnie updated the ec2 code

--- ec2-module.tf
+++ ec2-module.tf
@@ -1,6 +1,6 @@
 resource "aws_instance"  "Monda-application-server" {
     ami = data.aws_ami.amazon-linux-2.id
-    instance_type = "t2-small"
+    instance_type = "t2.micro"
     key_name = "Monda-application"
     subnet_id = aws_subnet.public-subnet-1.id
     associate_public_ip_address = true
